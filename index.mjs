594386f979b59f4f212f9fadfc05a98684fec237
Luan van Pletsen <luanvanpletsen@gmail.com>
2023-11-13 12:38:28 +0000

    Load mujoco_wasm

--- index.mjs
+++ index.mjs
@@ -1,12 +1,15 @@
-import { Model, Simulation, State, downloadFile } from "mujoco-wasm";
+import load_mujoco from "./mujoco_wasm/dist/mujoco_wasm.js";
 
 // Load the MuJoCo Module
-const mujoco = load_mujoco();
+const mujoco = await load_mujoco();
 
 // Set up Emscripten's Virtual File System
-mujoco.FS.mount(mujoco.MEMFS, { root: '.' }, '/examples');
+var initialScene = "drop_test.xml";
+mujoco.FS.mkdir('/working');
+mujoco.FS.mount(mujoco.MEMFS, { root: '.' }, '/working');
+mujoco.FS.writeFile("/working/" + initialScene, await(await fetch("./" + initialScene)).text());
 
 // Load in the state from XML
-let model       = new mujoco.Model("/examples/humanoid.xml");
+let model       = new mujoco.Model("/working/drop_test.xml");
 let state       = new mujoco.State(model);
 let simulation  = new mujoco.Simulation(model, state);
