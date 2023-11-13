946183a9d95cf7c7942b2cc0248ea70ec0898dda
Luan van Pletsen <luanvanpletsen@gmail.com>
2023-11-13 18:01:58 +0000

    Use bun

--- index.mjs
+++ index.mjs
@@ -1,15 +1,6 @@
-import load_mujoco from "./mujoco_wasm/dist/mujoco_wasm.js";
+import load_mujoco from 'mujoco-wasm';
 
-// Load the MuJoCo Module
 const mujoco = await load_mujoco();
 
-// Set up Emscripten's Virtual File System
-var initialScene = "drop_test.xml";
-mujoco.FS.mkdir('/working');
-mujoco.FS.mount(mujoco.MEMFS, { root: '.' }, '/working');
-mujoco.FS.writeFile("/working/" + initialScene, await(await fetch("./" + initialScene)).text());
+console.log('index.mjs');
 
-// Load in the state from XML
-let model       = new mujoco.Model("/working/drop_test.xml");
-let state       = new mujoco.State(model);
-let simulation  = new mujoco.Simulation(model, state);
